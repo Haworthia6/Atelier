40f0db8b76571cc6d5c05e3c063ecc2e9b2540a9
Kenny Tran <kenny.tran138@gmail.com>
2021-04-14 11:09:50 -0500

    Created frontend skeleton

---
+++ client/src/components/Test.jsx
@@ -0,0 +1,10 @@
+// import React, { useState } from 'react';
+
+// function Test () {
+//  const [names, setNames] = useState('Erin, Eren, and Kenny')
+//   return (
+//     <div>Your React Component is Mounted, {names}!</div>
+//   )
+// }
+
+// export default Test;
