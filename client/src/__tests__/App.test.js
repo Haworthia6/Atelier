112f58025beae362a9a9492d180eb302f9a214b2
Kenny Tran <kenny.tran138@gmail.com>
2021-04-17 11:36:30 -0500

    implemented chai-enzyme

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import chai from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+const expect = chai.expect
+chai.use(chaiEnzyme())
 
 import App from '../App'
 
@@ -9,6 +13,6 @@ describe('App', () => {
       <App />
     )
 
-    expect(wrapper.exists()).toBeTruthy()
+    expect(wrapper.exists()).to.be.true
   })
 })
