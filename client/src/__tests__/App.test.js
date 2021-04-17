dec343a474c94780c6b6c5c868ca783ae90fd4cf
Kenny Tran <kenny.tran138@gmail.com>
2021-04-17 11:46:24 -0500

    Implemented Chai-Enzyme test and replaced Chai-Jest syntax

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -8,11 +8,36 @@ chai.use(chaiEnzyme())
 import App from '../App'
 
 describe('App', () => {
-  test('should render', () => {
-    const wrapper = shallow(
-      <App />
-    )
+  let wrap;
 
-    expect(wrapper.exists()).to.be.true
+  beforeEach(() => {
+    wrap = shallow(<App status={'complete'} />)
   })
+
+  it('should exist', () => {
+    expect(wrap.exists()).to.be.true
+  });
+
+  it('should mount some components', () => {
+    expect(wrap.isEmptyRender()).to.be.false
+  });
+
+  it('should render a div', () => {
+    expect(wrap.find('div')).to.have.lengthOf(1)
+  });
+
+  xit('should successfully make an API request', () => {
+    wrap.find('button').invoke('onClick')().then(({data}) => {
+      expect(data).to.have.own.property('abilities')
+    })
+  });
+
+  it('should dynamically render text', () => {
+    expect(wrap.find('div').text()).to.include('React')
+  });
+
+  xit('should fail with this test', () => {
+    expect(wrap.isEmptyRender()).to.be.true
+  })
+
 })
