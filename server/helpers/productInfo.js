f1e7749d34afdbd79899ca754255118403b667fb
Eren Serpin <erenserpin@gmail.com>
2021-04-19 22:55:21 -0400

    Removed console log of api key

--- server/helpers/productInfo.js
+++ server/helpers/productInfo.js
@@ -2,7 +2,6 @@ const axios = require('axios');
 require('dotenv').config();
 
 module.exports = (id) => {
-  console.log('API KEY: ', process.env.API_KEY);
   return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${id}`,
     {
       headers: {
