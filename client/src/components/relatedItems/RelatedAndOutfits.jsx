c08d80a2dbd578b527cbb10b35a14e83edb67193
Eren Serpin <erenserpin@gmail.com>
2021-04-21 16:38:41 -0400

    Deleted bundle

--- client/src/components/relatedItems/RelatedAndOutfits.jsx
+++ client/src/components/relatedItems/RelatedAndOutfits.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import RelatedItems from './RelatedItems';
 import Outfits from './Outfits';
@@ -7,14 +7,37 @@ import useRelatedProductsIds from './custom/useRelatedProductsIds';
 
 function RelatedAndOutfits () {
   const currentProdId = useSelector((state) => state.currentProductId);
-  const products = useSelector((state) => state.products, shallowEqual);
+  const products = useSelector((state) => state.products);
   const relatedProductsIds = useRelatedProductsIds(currentProdId, products);
+  const [toggleComparing, setToggleComparing] = useState('fade-out'); // Handle turning back to false on close
+  const [comparedProducts, setComparedProducts] = useState({});
+
+  const handleComparingToggle = (relatedId) => {
+
+    setComparedProducts({
+      current: products[currentProdId],
+      related: products[relatedId]
+    });
+  };
 
   return (
-    <div>
-      <RelatedItems relatedProductsIds={relatedProductsIds}/>
-      {/* <Outfits /> */}
-      {/* <Comparing /> */}
+    <div id="related-items-and-outfits-component">
+      <button onClick={() => setToggleComparing('fade-out')}>toggle</button>
+      <h2>RELATED PRODUCTS</h2>
+      <RelatedItems
+        relatedProductsIds={relatedProductsIds}
+        products={products}
+        setToggleComparing={setToggleComparing}
+        handleComparingToggle={handleComparingToggle}
+      />
+      {/* <h2>YOUR OUTFIT</h2>
+      <Outfits /> */}
+      <div id="comparing-position">
+        <Comparing
+          visibility={toggleComparing}
+          comparedProducts={comparedProducts}
+        />
+      </div>
     </div>
   );
 }
