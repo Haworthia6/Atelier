1a3d021dcd1a06fa5e32762b4bceca5cfdfc050c
Kenny Tran <kenny.tran138@gmail.com>
2021-04-23 12:24:44 -0500

    Removed counter from application which was a test component for Redux

--- client/store/reducers/mainReducer.js
+++ client/store/reducers/mainReducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import counterReducer from './counterReducer';
 import productIdReducer from './productIdReducer';
 import productsReducer from './productsReducer';
 import outfitsReducer from './outfitsReducer';
 import toggleShowReducer from './toggleShowReducer';
 
 const rootReducer = combineReducers({
-  count: counterReducer,
   currentProductId: productIdReducer,
   products: productsReducer,
   outfits: outfitsReducer,
