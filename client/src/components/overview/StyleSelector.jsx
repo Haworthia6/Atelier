84e0dadd442648cb76578d28cff9770422ba856c
Eren Serpin <erenserpin@gmail.com>
2021-04-24 15:37:02 -0400

    Separated styles into rows of 4 and made them into circles

--- client/src/components/overview/StyleSelector.jsx
+++ client/src/components/overview/StyleSelector.jsx
@@ -1,23 +1,52 @@
 import React, {useState} from 'react';
-import { useSelector, useDispatch } from 'react-redux'
 
 function StyleSelector ({styleList, currentStyle, onStyleChange}) {
-  const dis = useDispatch();
+  // divide styleList into an array of arrays of 4
+  const splitArray = (array) => {
+    var styleRows = [];
+    var rows = Math.ceil(array.length / 4);
+    console.log(rows);
+    var currentEle = 0;
+    // iterate over rows
+    // if current row is not last row, add an array of the next 4 elements to split
+    // if current row is last row, push elements into an array until element index is array.length
+    for (var i = 0; i < rows; i++)  {
+      var row = [];
 
+      if (i !== rows - 1) {
+        for (var j = 0; j < 4; j++) {
+          row.push(array[currentEle]);
+          currentEle ++;
+        }
+      } else {
+        while(currentEle !== array.length) {
+          row.push(array[currentEle]);
+          currentEle ++;
+        }
+      }
+      styleRows.push(row);
+      console.log(styleRows);
+    }
+    return styleRows;
+  };
   return (
     <div>
-      <p>Style> {styleList[currentStyle].name}</p>
-      {styleList.map((style, index) => {
-        if (index === currentStyle) {
-          console.log(styleList[currentStyle]);
-          return <img src={style.photos[0].thumbnail_url} alt="current style" className ="styleThumbnail" key={index} id={index} onClick={onStyleChange}/>
-        } else {
-          return <img src={style.photos[0].thumbnail_url} alt="style" className ="styleThumbnail" id={index} key={index} onClick={onStyleChange}/>
-        }
+      <p>Style{'>'} {styleList[currentStyle].name}</p>
+      {splitArray(styleList).map((row, rowIndex) => {
+        return (
+          <div key={rowIndex}>
+            {row.map((style, styleIndex) => {
+              if (style.style_id === currentStyle) {
+                return <img src={style.photos[0].thumbnail_url} alt="current style" className ="styleThumbnail" key={styleIndex} id={(rowIndex * 4) + styleIndex} onClick={onStyleChange}/>;
+              } else {
+                return <img src={style.photos[0].thumbnail_url} alt="style" className ="styleThumbnail" id={(rowIndex * 4) + styleIndex} key={styleIndex} onClick={onStyleChange}/>;
+              }
+            })}
+          </div>);
       })}
     </div>
 
-  )
+  );
 }
 
 export default StyleSelector;
