49d1895a10a2dfca57e35b44308e895d2091d4be
Eren Serpin <erenserpin@gmail.com>
2021-04-24 14:30:59 -0400

    Implemented share buttons

--- client/src/components/overview/ProductInfo.jsx
+++ client/src/components/overview/ProductInfo.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import {GrFacebook} from "react-icons/gr";
+import {GrFacebook} from 'react-icons/gr';
+import { AiFillTwitterSquare } from 'react-icons/ai';
+import { FaPinterestSquare } from 'react-icons/fa';
+import {FacebookShareButton, TwitterShareButton, PinterestShareButton} from 'react-share';
 
 function ProductInfo (props) {
   const renderPrice = () => {
@@ -22,11 +25,32 @@ function ProductInfo (props) {
       <p>{props.name}</p>
       {renderPrice()}
       <div>
-        share buttons
-        <button className="share"></button>
+        {/*NEED TO CHANGE URLS TO THE CORRECT ONE*/}
+        <span className="share">
+          <FacebookShareButton
+            url="http://localhost:3000/"
+            quote="Buy this instantly">
+            <GrFacebook/>
+          </FacebookShareButton>
+        </span>
+        <span className="share">
+          <TwitterShareButton
+            url="http://localhost:3000/"
+            title="Haworthia">
+            <AiFillTwitterSquare/>
+          </TwitterShareButton>
+        </span>
+        <span className="share">
+          <PinterestShareButton
+            url="http://localhost:3000/"
+            media="http://github.com"
+            description='This piece is dope'>
+            <FaPinterestSquare/>
+          </PinterestShareButton>
+        </span>
       </div>
     </div>
-  )
+  );
 }
 
 export default ProductInfo;
