e58e14e9adde6c258a97fb0fafd4b6462ae41c9a
Eren Serpin <erenserpin@gmail.com>
2021-04-25 22:29:00 -0400

    quick lintin pt.2

--- client/src/components/overview/StyleThumbnail.jsx
+++ client/src/components/overview/StyleThumbnail.jsx
@@ -5,12 +5,22 @@ const StyleThumbnail = (({style, styleList, onStyleChange, currentStyle, id}) =>
   if (style.style_id === styleList[currentStyle].style_id) {
     return (<div className="selectedStyleContainer" >
       <IoMdCheckmarkCircleOutline className="overlay"/>
-      <img src={style.photos[0].thumbnail_url} alt="current style" className="styleThumbnail" id={id} onClick={onStyleChange}/>
+      <img
+        src={style.photos[0].thumbnail_url}
+        alt="current style"
+        className="styleThumbnail"
+        id={id}
+        onClick={onStyleChange}/>
     </div>);
   } else {
     return (
       <div className="selectedStyleContainer">
-        <img src={style.photos[0].thumbnail_url} alt="style" className ="styleThumbnail" id={id} onClick={onStyleChange}/>
+        <img
+          src={style.photos[0].thumbnail_url}
+          alt="style"
+          className ="styleThumbnail"
+          id={id}
+          onClick={onStyleChange}/>
       </div>);
   }
 });
