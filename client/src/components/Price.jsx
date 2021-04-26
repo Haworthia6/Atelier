3a6d9eb7b888176b752c4f37c3c1b16e40cf7414
Kenny Tran <kenny.tran138@gmail.com>
2021-04-26 13:32:26 -0500

    Refactored Price component and added Prop Checking

--- client/src/components/Price.jsx
+++ client/src/components/Price.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { isNull, isString } from 'lodash';
 
-const Price = (({price, salePrice}) => {
-  if (salePrice) {
-    return (
-      <div>
+const Price = (({ price, salePrice }) => (
+  <div className="price-container">
+    { salePrice ?
+      (<>
         <p className="crossed-out-price">{price}</p>
-        <p>{salePrice}</p>
-      </div>);
-  } else {
-    return <p className="price">{price}</p>;
-  }
-});
+        <p className="sale-price">{salePrice}</p>
+      </>)
+      : <p className="price">{price}</p>
+    }
+  </div>
+));
+
+// Prop Checking --------------
+Price.propTypes = {
+  price: PropTypes.string.isRequired,
+  salePrice: (({salePrice}) => {
+    if (isNull(salePrice)) return;
+    if (!isString(salePrice)) {
+      throw new Error(`Expected salePrice to be type string but it is ${typeof salePrice}`);
+    }
+  })
+};
+
 export default Price;
